a283eecccfeb3351284275af8cd23f7129258228
mehboobb <118979859+mehboobb@users.noreply.github.com>
2026-06-28 12:40:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,19 @@ function validateForm() {
     error.style.color = "green";
     return false; // Prevents page refresh for now
 }
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value.trim();
+
+    if (task === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
