32d75c975d0581d560e2d1df6450966f31ee7e1b
mehboobb <118979859+mehboobb@users.noreply.github.com>
2026-06-28 10:48:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function showMessage() {
     document.getElementById("message").textContent =
         "Welcome to Mehboob Bugti's Portfolio!";
 }
+function updateClock() {
+    const now = new Date();
+    document.getElementById("clock").textContent =
+        now.toLocaleTimeString();
+}
+
+setInterval(updateClock, 1000);
+updateClock();
