79f600200018a102a1ea920a8cbe828a2f61a60b
mehboobb <118979859+mehboobb@users.noreply.github.com>
2026-06-28 10:42:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
 }
+function showMessage() {
+    document.getElementById("message").textContent =
+        "Welcome to Mehboob Bugti's Portfolio!";
+}
